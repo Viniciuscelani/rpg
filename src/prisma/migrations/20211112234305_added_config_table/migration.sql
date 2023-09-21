c11265e66b0385fe457d938494c51d0664542b4f
Vinicius Celani <89923497+Viniciuscelani@users.noreply.github.com>
2023-09-21 17:16:26 -0400

    Update migration.sql

--- src/prisma/migrations/20211112234305_added_config_table/migration.sql
+++ src/prisma/migrations/20211112234305_added_config_table/migration.sql
@@ -1,8 +1,6 @@
 -- CreateTable
-CREATE TABLE `config` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `value` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE TABLE "config" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(191) NOT NULL,
+    "value" VARCHAR(191)
+)  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
